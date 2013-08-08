50d7c32152469f81f3e049d17af41d1df93401e9
Aishwarya Kumar <kumar.aish2010@gmail.com>
2013-08-09 01:59:07 +0530

    contextual menu on list items

--- OscTracker/src/com/aishwarya/osctracker/OscItemsListFragment.java
+++ OscTracker/src/com/aishwarya/osctracker/OscItemsListFragment.java
@@ -1,7 +1,45 @@
 package com.aishwarya.osctracker;
 
+import android.os.Bundle;
 import android.support.v4.app.ListFragment;
+import android.util.Log;
+import android.view.ContextMenu;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView.AdapterContextMenuInfo;
 
 public class OscItemsListFragment extends ListFragment {
+
+	@Override
+	public void onActivityCreated(Bundle savedInstanceState) {
+		super.onActivityCreated(savedInstanceState);
+
+		registerForContextMenu(getListView());
+	}
+
+	@Override
+	public void onCreateContextMenu(ContextMenu menu, View v,
+			ContextMenu.ContextMenuInfo menuInfo) {
+		super.onCreateContextMenu(menu, v, menuInfo);
+
+		menu.add(0, Menu.FIRST, 0, "Delete");
+		menu.add(0, Menu.FIRST + 1, 0, "Edit");
+	}
+
+	@Override
+	public boolean onContextItemSelected(MenuItem item) {
+		switch (item.getItemId()) {
+		case Menu.FIRST:
+			Log.d("aish", "deletemenu");
+			return true;
+		case Menu.FIRST + 1:
+			Log.d("aish", "editmenu");
+			return true;
+		default:
+
+			return super.onContextItemSelected(item);
+		}
+	}
 
 }
